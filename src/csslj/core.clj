7ccc6e47fcd244bdca074c6df79c9ae18ee1ebc5
Curtis (Programble) <programble@gmail.com>
2010-06-02 16:02:35 -0400

    Well that was easy

--- src/csslj/core.clj
+++ src/csslj/core.clj
@@ -1,1 +1,19 @@
 (ns csslj.core)
+
+(defn- render-ele [ele]
+  (->> ele (map name) (interpose " ") (apply str)))
+
+(defn- render-attrs [attrs]
+  (apply str
+         (sort
+          (for [[attr value] attrs]
+            (str (name attr) ": " value ";\n")))))
+
+(defn css
+  "Takes any amount of vectors of CSS elements and renders them as CSS"
+  ([vec]
+     (let [ele (render-ele (drop-last vec))
+           attrs (render-attrs (last vec))]
+       (str ele " {\n" attrs "}")))
+  ([vec & more]
+     (->> (map css (cons vec more)) (interpose "\n") (apply str))))
