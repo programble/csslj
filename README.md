a49caeb9af9d4fa70b5c5760911f06a1b134e366
Curtis (Programble) <programble@gmail.com>
2010-06-02 15:19:59 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# CSSLJ
+
+CSSLJ is a library for representing CSS in Clojure in the same style
+as [Hiccup](http://github.com/weavejester/hiccup).
+
+## Syntax
+
+FIXME: write
+
+## License
+
+FIXME: write
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject csslj "1.0.0-SNAPSHOT"
+  :description "A library for representing CSS in Clojure"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"]
+                     [lein-search "0.3.0-SNAPSHOT"]])
